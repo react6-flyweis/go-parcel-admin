88b031db6a5bd3cc86272b2206e7e169dcde5283
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-01-20 18:35:49 +0530

    feat: enhance StatCard component with customizable icon direction and improved layout

--- src/components/ui/stat-card.tsx
+++ src/components/ui/stat-card.tsx
@@ -6,6 +6,7 @@ interface StatCardProps {
   subtitle?: string | React.ReactNode;
   color?: string; // tailwind color class for the left accent bar
   icon?: React.ReactNode;
+  iconDirection?: "left" | "right";
 }
 
 export default function StatCard({
@@ -14,24 +15,34 @@ export default function StatCard({
   subtitle,
   color = "bg-gray-400",
   icon,
+  iconDirection = "left",
 }: StatCardProps) {
+  const iconNode = (
+    <div
+      className={`h-12 rounded-md flex justify-center items-center ${color} ${
+        icon ? "w-12" : "w-2"
+      }`}
+    >
+      {icon}
+    </div>
+  );
+
   return (
-    <Card>
-      <CardContent className="flex items-center gap-4">
-        <div
-          className={`h-12 rounded-md flex justify-center items-center ${color} ${
-            icon ? "w-12" : "w-2"
-          }`}
-        >
-          {icon}
-        </div>
+    <Card className="py-5">
+      <CardContent className="flex items-center gap-4 px-3">
+        {iconDirection === "left" && iconNode}
+
         <div className="flex-1">
           <div className="text-sm text-muted-foreground">{title}</div>
-          <div className="text-2xl font-bold">{value}</div>
+          <div className="text-2xl ">{value}</div>
           {subtitle && (
-            <div className="text-sm text-muted-foreground">{subtitle}</div>
+            <div className="text-xs font-medium text-muted-foreground">
+              {subtitle}
+            </div>
           )}
         </div>
+
+        {iconDirection === "right" && iconNode}
       </CardContent>
     </Card>
   );
